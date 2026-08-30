49833762448e2644df68dfa08f87b221b576a292
Gungun Singh <gungunsingh06082005@gmail.com>
2026-08-30 22:17:43 +0530

    invalidate user session by removing refresh token on logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -161,8 +161,9 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1,  //this removes the refresh token from the user document in the database, effectively logging the user out by invalidating their session
+
       },
     },
     {
